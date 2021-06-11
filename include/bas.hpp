8f14c9c77167426cc025ba036619f5aae322061c
Nell Fauveau <nfauveau@nell.fauveau>
2021-06-11 12:25:39 +0200

    fix(documentation): fixed a typo in the main example of the documentation

--- include/bas.hpp
+++ include/bas.hpp
@@ -634,53 +634,53 @@ public:
  * @mainpage
  * Simple example to start with:
  * @code
- *#include "bas.hpp"
- *
- *#include <string>
- *
- *class SerializablePerson : public bas::Serializable {                        // inherit from the Serializable class
- *public:
- *    SerializablePerson(const std::string& name, int age)
- *        : name(name)
- *        , age(age)
- *    {
- *    }
- *
- *    ~SerializablePerson() = default;
- *
- *    // this function contains the serialization process of your class
- *    void makeSerialization(bas::SerializedObject& obj) override
- *    {
- *        obj.pushData(name);                                                // Notice that the library contains a custom variant of pushData and popData for std::string
- *        obj.pushData(age);
- *    }
- *
- *    // this function contains the unserialization process of your class
- *    void makeUnserialization(bas::SerializedObject& obj) override
- *    {
- *        name = obj.popData<std::string>();
- *        age = obj.popData<int>();
- *    }
- *
- *    std::string name;
- *    int age;
- *};
- *
- *int main(void)
- *{
- *    SerializablePerson person1("David", 32);
- *    SerializablePerson person2("Robert", 45);
- *
- *    bas::SerializedObject obj;
- *
- *    obj = person1.serialize();                                             // Use serialize() and not makeSerialization()
- *
- *    person2.unserialize(obj);                                              // Use unserialize() and not makeUnserialization()
- *
- *    // person2 is now a copy of person1
- *
- *    return 0;
- *}
+#include "bas.hpp"
+
+#include <string>
+
+class SerializablePerson : public bas::Serializable {                        // inherit from the Serializable class
+public:
+    SerializablePerson(const std::string& name, int age)
+        : name(name)
+        , age(age)
+    {
+    }
+
+    ~SerializablePerson() = default;
+
+    // this function contains the serialization process of your class
+    void makeSerialization(bas::SerializedObject& obj) override
+    {
+        obj.pushData(name);                                                // Notice that the library contains a custom variant of pushData and popData for std::string
+        obj.pushData(age);
+    }
+
+    // this function contains the unserialization process of your class
+    void makeUnserialization(bas::SerializedObject& obj) override
+    {
+        name = obj.popData<std::string>();
+        age = obj.popData<int>();
+    }
+
+    std::string name;
+    int age;
+};
+
+int main(void)
+{
+    SerializablePerson person1("David", 32);
+    SerializablePerson person2("Robert", 45);
+
+    bas::SerializedObject obj;
+
+    obj = person1.serialize();                                             // Use serialize() and not makeSerialization()
+
+    person2.unserialize(obj);                                              // Use unserialize() and not makeUnserialization()
+
+    // person2 is now a copy of person1
+
+    return 0;
+}
  *@endcode
  */
 #endif /* !BAS_HPP_ */
